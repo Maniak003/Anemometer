5790b85f28d81651e4e1411cbadc85e973b47a0d
Maniak003 <ed@ultrastar.ru>
2023-06-12 17:36:18 +0300

    новый файл:    stm32cubeide/Anemometer_G474CBT6_MATLAB/Core/Inc/maxEnvHilbert_data.h

---
+++ stm32cubeide/Anemometer_G474CBT6_MATLAB/Core/Inc/maxEnvHilbert_data.h
@@ -0,0 +1,21 @@
+/*
+ * File: maxEnvHilbert_data.h
+ *
+ * MATLAB Coder version            : 5.5
+ * C/C++ source code generated on  : 27-May-2023 12:28:36
+ */
+
+#ifndef MAXENVHILBERT_DATA_H
+#define MAXENVHILBERT_DATA_H
+
+/* Include Files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#endif
+/*
+ * File trailer for maxEnvHilbert_data.h
+ *
+ * [EOF]
+ */
